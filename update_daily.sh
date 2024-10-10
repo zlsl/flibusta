281d53d21a6c50da2ada86b7e211d313ed89c4dd
Golets Alexey <goletsa@gmail.com>
2024-10-10 10:42:41 +0300

    Fix non fb2 books download
    Fix non fb2 books download.

--- update_daily.sh
+++ update_daily.sh
@@ -3,7 +3,7 @@ URL="http://flibusta.is/daily/"
 DEST_DIR="./Flibusta.Net"
 mkdir -p "$DEST_DIR"
 curl -s "$URL" > page.html
-grep -Eo 'href="f\.(fb2)\.[0-9\-]+\.zip"' page.html | sed 's/href="//;s/"//' > links.txt
+grep -Eo 'href="f\.(fb2|n)\.[0-9\-]+\.zip"' page.html | sed 's/href="//;s/"//' > links.txt
 
 while IFS= read -r file; do
     wget -c -P "$DEST_DIR" "$URL$file"
